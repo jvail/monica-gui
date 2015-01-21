62f34f6148836f3280de2ed2a5e210f35fcf76ad
jvail <jv@zalf.de>
2015-01-21 11:30:52 +0100

    Squashed 'monica/' content from commit 3b60834
    git-subtree-dir: monica
    git-subtree-split: 3b608349a310d662504ad189ab038a592b92fea7

---
+++ installer/installer-script.nsi
@@ -0,0 +1,232 @@
+;NSIS Modern User Interface
+;Start Menu Folder Selection Example Script
+;Written by Joost Verburg
+
+;--------------------------------
+;Include Modern UI
+
+  !include "MUI2.nsh"
+
+	!ifdef X86
+		!define Arch "x86"
+		!define ArchBit "32bit"
+	!else
+		!define Arch "x64"
+		!define ArchBit "64bit"
+	!endif
+	
+	!ifdef HERMES
+		!define MonicaVersion "hermes"
+	!else
+		!define MonicaVersion "json"
+	!endif
+	
+	!ifdef VC11
+		!define VCversion "11"
+	!else
+		!define VCversion "12"
+	!endif
+	
+;--------------------------------
+;General
+
+  ;Name and file
+  Name "MONICA"
+  OutFile "MONICA-Setup-1.2-${Arch}-${ArchBit}.exe"
+
+  ;Default installation folder
+  InstallDir "$PROGRAMFILES\MONICA"
+  
+  ;Get installation folder from registry if available
+  InstallDirRegKey HKCU "Software\MONICA" ""
+
+  ;Request application privileges for Windows Vista
+  RequestExecutionLevel admin
+
+;--------------------------------
+;Variables
+
+  Var StartMenuFolder
+
+;--------------------------------
+;Interface Settings
+
+  !define MUI_ABORTWARNING
+  !define MUI_HEADERIMAGE
+  !define MUI_HEADERIMAGE_LEFT
+  !define MUI_HEADERIMAGE_BITMAP "orange.bmp"
+  !define ICON		"monica_icon.ico"
+  !define MUI_ICON	"monica_icon.ico"
+
+;--------------------------------
+;Pages
+
+  !insertmacro MUI_PAGE_WELCOME
+  !insertmacro MUI_PAGE_LICENSE "license.txt"
+  !insertmacro MUI_PAGE_COMPONENTS
+  !insertmacro MUI_PAGE_DIRECTORY
+  
+  ;Start Menu Folder Page Configuration
+  !define MUI_STARTMENUPAGE_REGISTRY_ROOT "HKCU" 
+  !define MUI_STARTMENUPAGE_REGISTRY_KEY "Software\MONICA" 
+  !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "Start Menu Folder"
+  
+  !insertmacro MUI_PAGE_STARTMENU Application $StartMenuFolder
+  
+  !insertmacro MUI_PAGE_INSTFILES
+  
+  !insertmacro MUI_UNPAGE_CONFIRM
+  !insertmacro MUI_UNPAGE_INSTFILES
+
+;--------------------------------
+;Languages
+ 
+  !insertmacro MUI_LANGUAGE "German"
+
+;--------------------------------
+;Installer Sections
+
+Section "MONICA - Model for Nitrogen and Carbon in Agro-ecosystems" SecDummy
+
+	;SetOutPath "c:\temp"
+	;install Visual C++ 2012 runtime
+	;File "vcredist_${Arch}.exe"
+	;ExecWait "c:\temp\vcredist_${ARCH}.exe /q:a"
+	;Delete "c:\temp\vcredist_${ARCH}.exe"
+
+  SetOutPath "$INSTDIR"
+  
+  ;ADD YOUR OWN FILES HERE...
+  File /oname=monica.exe "..\release\monica-${MonicaVersion}.exe"  
+	File "C:\Program Files (x86)\Microsoft Visual Studio ${VCversion}.0\VC\redist\${Arch}\Microsoft.VC${VCversion}0.CRT\*.dll"
+	File "..\db-connections.ini"
+	File "license.txt"
+	File "..\documentation\de_benutzerhandbuch_MONICA_windows.pdf"
+	File "..\documentation\en_user_manual_MONICA_windows.pdf"
+  	File "..\documentation\version.txt"
+  
+  ;CreateDirectory "$PROFILE\MONICA\sqlite-db"
+  SetOutPath $INSTDIR\sqlite-db
+  File "..\sqlite-db\monica.sqlite"
+    
+	SetOutPath $INSTDIR\meta.json
+  File "..\meta.json\meta.crop.json"
+  File "..\meta.json\meta.sim.json"
+  File "..\meta.json\meta.site.json"
+  File "..\meta.json\howto.meta.txt"
+	
+  CreateDirectory $PROFILE\MONICA\Examples\Hohenfinow2
+  SetOutPath $PROFILE\MONICA\Examples\Hohenfinow2
+  File "Hohenfinow2\monica.ini"
+  File "Hohenfinow2\Irrig.TXT"
+  File "Hohenfinow2\MET_HF.991"
+  File "Hohenfinow2\MET_HF.992"
+  File "Hohenfinow2\MET_HF.993"
+  File "Hohenfinow2\MET_HF.994"
+  File "Hohenfinow2\MET_HF.995"
+  File "Hohenfinow2\MET_HF.996"
+  File "Hohenfinow2\MET_HF.997"
+  
+  File "Hohenfinow2\ROTATION.txt"
+  File "Hohenfinow2\SLAGDUNG.txt"
+  File "Hohenfinow2\SOIL.txt"
+
+  CreateDirectory $PROFILE\MONICA\Examples\Hohenfinow2\json
+  SetOutPath $PROFILE\MONICA\Examples\Hohenfinow2\json
+  File "Hohenfinow2\json\test.crop.json"
+  File "Hohenfinow2\json\test.sim.json"
+  File "Hohenfinow2\json\test.site.json"
+  	
+  SetOutPath "$INSTDIR"
+  
+  ;Store installation folder
+  WriteRegStr HKCU "Software\MONICA" "" $INSTDIR
+  
+  ;Create uninstaller
+  WriteUninstaller "$INSTDIR\Uninstall.exe"
+  
+  !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+    
+	;Create shortcuts
+    CreateDirectory "$SMPROGRAMS\$StartMenuFolder"
+	CreateShortCut "$SMPROGRAMS\$StartMenuFolder\Uninstall.lnk" "$INSTDIR\Uninstall.exe"	
+	CreateShortCut "$SMPROGRAMS\$StartMenuFolder\MONICA.lnk" "$INSTDIR\monica.exe" "$\"%USERPROFILE%$\"\MONICA\Examples\Hohenfinow2"
+	CreateShortCut "$SMPROGRAMS\$StartMenuFolder\Documentation MONICA for Windows.lnk" "$INSTDIR\en_user_manual_MONICA_windows.pdf"	
+	CreateShortCut "$SMPROGRAMS\$StartMenuFolder\Dokumentation MONICA for Windows.lnk" "$INSTDIR\de_benutzerhandbuch_MONICA_windows.pdf"
+	;CreateShortCut "$SMPROGRAMS\$StartMenuFolder\Modellbeschreibung MONICA for Windows.lnk" "$INSTDIR\Modellbeschreibung_MONICA_Version_1-1-1.pdf"		
+	CreateShortCut "$DESKTOP\MONICA.lnk" "$INSTDIR\monica.exe" "$\"%USERPROFILE%$\"\MONICA\Examples\Hohenfinow2"
+	
+  !insertmacro MUI_STARTMENU_WRITE_END
+
+SectionEnd
+
+;--------------------------------
+;Descriptions
+
+  ;Language strings
+  LangString DESC_SecDummy ${LANG_GERMAN} "MONICA - Model for Nitrogen and Carbon in Agro-ecosystems"
+
+  ;Assign language strings to sections
+  !insertmacro MUI_FUNCTION_DESCRIPTION_BEGIN
+    !insertmacro MUI_DESCRIPTION_TEXT ${SecDummy} $(DESC_SecDummy)
+  !insertmacro MUI_FUNCTION_DESCRIPTION_END
+ 
+;--------------------------------
+;Uninstaller Section
+
+Section "Uninstall"
+
+  Delete "$INSTDIR\monica.exe"
+  Delete "$INSTDIR\db-connections.ini"
+  ;Delete "$INSTDIR\*.dll"
+  ;Delete "$INSTDIR\climate.dll"
+  ;Delete "$INSTDIR\tools.dll"
+  ;Delete "$INSTDIR\db.dll"
+  ;Delete "$INSTDIR\libmysql.dll"  
+  ;Delete "$INSTDIR\mingwm10.dll"  
+  ;Delete "$INSTDIR\libgcc_s_dw2-1.dll"
+  Delete "$INSTDIR\gmon.out"
+  Delete "$INSTDIR\license.txt"
+  ;Delete "$INSTDIR\Readme.pdf"
+	Delete "$INSTDIR\en_user_manual_MONICA_windows.pdf"	
+	Delete "$INSTDIR\de_benutzerhandbuch_MONICA_windows.pdf"
+	Delete "$INSTDIR\version.txt"
+	
+  Delete "$INSTDIR\sqlite-db\monica.sqlite"
+  RMDir $INSTDIR\sqlite-db
+  
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\monica.ini
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\Irrig.TXT
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.991
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.992
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.993
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.994
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.995
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.996
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\MET_HF.997  
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\ROTATION.txt
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\rmout.dat
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\smout.dat
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\gmon.out
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\SLAGDUNG.txt
+  Delete $PROFILE\MONICA\Examples\Hohenfinow2\SOIL.txt
+  
+  RMDir  $PROFILE\MONICA\Examples\Hohenfinow2
+  RMDir  $PROFILE\MONICA\Examples
+  RMDir  $PROFILE\MONICA
+  
+  Delete "$INSTDIR\Uninstall.exe"
+
+  RMDir "$INSTDIR"
+  
+  !insertmacro MUI_STARTMENU_GETFOLDER Application $StartMenuFolder
+    
+  Delete "$SMPROGRAMS\$StartMenuFolder\MONICA.lnk"
+  Delete "$SMPROGRAMS\$StartMenuFolder\Uninstall.lnk"
+  Delete "$SMPROGRAMS\$StartMenuFolder\Readme.lnk"
+  Delete "$DESKTOP\MONICA.lnk"
+  RMDir "$SMPROGRAMS\$StartMenuFolder"
+  
+  DeleteRegKey /ifempty HKCU "Software\MONICA"
+
+SectionEnd
---
+++ src/monica.h
@@ -0,0 +1,445 @@
+/**
+Authors: 
+Dr. Claas Nendel <claas.nendel@zalf.de>
+Xenia Specka <xenia.specka@zalf.de>
+Michael Berg <michael.berg@zalf.de>
+
+Maintainers: 
+Currently maintained by the authors.
+
+This file is part of the MONICA model. 
+Copyright (C) 2007-2013, Leibniz Centre for Agricultural Landscape Research (ZALF)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MONICA_H_
+#define MONICA_H_
+
+
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <map>
+
+#include "climate/climate-common.h"
+#include "soilcolumn.h"
+#include "soiltemperature.h"
+#include "soilmoisture.h"
+#include "soilorganic.h"
+#include "soiltransport.h"
+#include "crop.h"
+#include "tools/date.h"
+#include "tools/datastructures.h"
+#include "monica-parameters.h"
+#include "tools/helper.h"
+
+namespace Monica
+{
+	/* forward declaration */
+	class Configuration;
+
+  /**
+   * @class Env
+   */
+  struct Env 
+  {
+    /**
+     * @brief
+     */
+		enum
+		{
+			MODE_LC_DSS = 0,
+      MODE_ACTIVATE_OUTPUT_FILES,
+			MODE_HERMES,
+			MODE_EVA2,
+			MODE_SENSITIVITY_ANALYSIS,
+			MODE_CC_GERMANY,
+			MODE_MACSUR_SCALING
+		};
+
+    /**
+     * @brief default constructor for value object use
+     */
+		Env()
+			: soilParams(NULL),
+				noOfLayers(0),
+				layerThickness(0),
+				useNMinMineralFertilisingMethod(false),
+				useAutomaticIrrigation(false),
+				useSecondaryYields(true),
+				atmosphericCO2(-1),
+				customId(-1)
+		{ }
+
+    Env(const Env&);
+		    
+    /**
+     * @brief Constructor
+     * @param sps Soil parameters
+     * @param nols Number of layers
+     * @param lt Layer thickness
+     */
+    Env(const SoilPMs* sps, CentralParameterProvider cpp);
+
+		//use this version if soil parameters can't be safely kept elsewhere and Env is responsible for deleting them
+		Env(SoilPMsPtr sps, CentralParameterProvider cpp);
+
+    int numberOfPossibleSteps();
+    void addOrReplaceClimateData(std::string, const std::vector<double>& data);
+
+	private:
+		SoilPMsPtr _soilParamsPtr;
+	public:
+    const SoilPMs* soilParams;  //! a vector of soil parameter objects = layers of soil
+
+    unsigned int noOfLayers;              //! number of layers
+    double layerThickness;                //! thickness of a single layer
+
+    bool useNMinMineralFertilisingMethod;
+    MineralFertiliserParameters nMinFertiliserPartition;
+    NMinUserParameters nMinUserParams;
+
+    bool useAutomaticIrrigation;
+    AutomaticIrrigationParameters autoIrrigationParams;
+    MeasuredGroundwaterTableInformation groundwaterInformation;
+
+    bool useSecondaryYields;              //! tell if farmer uses the secondary yield products
+
+    //static const int depthGroundwaterTable;   /**<  */
+    double windSpeedHeight; /**<  */
+    double atmosphericCO2; /**< [ppm] */
+    double albedo; /**< Albedo [] */
+
+    Climate::DataAccessor da;     //! object holding the climate data
+    std::vector<ProductionProcess> cropRotation; //! vector of elements holding the data of the single crops in the rotation
+
+    Tools::GridPoint gridPoint;        //! the gridpoint the model runs, just a convenience for the dss use
+		int customId;
+
+    SiteParameters site;        //! site specific parameters
+    GeneralParameters general;  //! general parameters to the model
+		OrganicConstants organic;  //! constant organic parameters to the model
+
+    CentralParameterProvider centralParameterProvider;
+
+    std::string toString() const;
+		std::string pathToOutputDir;
+
+		void setMode(int mode);
+		int getMode() {return this->mode; }
+
+		void setCropRotation(std::vector<ProductionProcess> ff)
+		{
+		  cropRotation = ff;
+		}
+
+    private:
+      int mode;   //! Variable for differentiate betweend execution modes of MONICA
+  };
+
+  //----------------------------------------------------------------------------
+  
+  /*!
+   * @brief structure holding all results of one monica run
+   */
+  class Result 
+  {
+    public:
+      Result(){}
+      ~Result(){}
+
+    //! just to keep track of the grid point the calculation is being made for
+    Tools::GridPoint gp;
+		//is as gp before used to track results when multiple parallel
+		//unordered invocations of monica will happen
+		int customId;
+
+    //! vector of the result of one crop per year
+		std::vector<PVResult> pvrs;
+
+    //! results not regarding a particular crop in a rotation
+    typedef std::map<ResultId, std::vector<double> > RId2Vector;
+    RId2Vector generalResults;
+
+    std::vector<double> getResultsById(int id);
+
+		int sizeGeneralResults() { return int(generalResults.size()); }
+
+    std::vector<std::string> dates;
+
+    std::string toString();
+  };
+
+  //----------------------------------------------------------------------------
+
+  /*!
+   * @brief Core class of MONICA
+   * @author Claas Nendel, Michael Berg
+   */
+	class MonicaModel
+	{
+  public:
+    MonicaModel(const Env& env, const Climate::DataAccessor& da);
+
+    /**
+     * @brief Destructor
+     */
+		~MonicaModel()
+		{
+      delete _currentCropGrowth;
+    }
+
+    void generalStep(unsigned int stepNo);
+    void cropStep(unsigned int stepNo);
+    double CO2ForDate(double year, double julianDay, bool isLeapYear);
+    double GroundwaterDepthForDate(double maxGroundwaterDepth,
+			    double minGroundwaterDepth,
+			    int minGroundwaterDepthMonth,
+			    double julianday,
+			    bool leapYear);
+
+
+    //! seed given crop
+    void seedCrop(CropPtr crop);
+
+    //! what crop is currently seeded ?
+		CropPtr currentCrop() const { return _currentCrop; }
+
+		bool isCropPlanted() const
+		{
+      return _currentCrop.get() && _currentCrop->isValid();
+    }
+
+    //! harvest the currently seeded crop
+		void harvestCurrentCrop(bool exported);
+		
+		//! harvest the fruit of the current crop
+		void fruitHarvestCurrentCrop(double percentage, bool exported);
+
+		//! prune the leaves of the current crop
+		void leafPruningCurrentCrop(double percentage, bool exported);
+
+		//! prune the tips of the current crop
+		void tipPruningCurrentCrop(double percentage, bool exported);
+
+		//! prune the shoots of the current crop
+		void shootPruningCurrentCrop(double percentage, bool exported);
+
+		//! prune the shoots of the current crop
+		void cuttingCurrentCrop(double percentage, bool exported);
+
+    void incorporateCurrentCrop();
+
+    void applyMineralFertiliser(MineralFertiliserParameters partition,
+                                double amount);
+
+    void applyOrganicFertiliser(const OrganicMatterParameters*, double amount,
+																bool incorporation);
+
+		bool useNMinMineralFertilisingMethod() const
+		{
+      return _env.useNMinMineralFertilisingMethod;
+    }
+
+    double applyMineralFertiliserViaNMinMethod
+        (MineralFertiliserParameters partition, NMinCropParameters cropParams);
+
+		double dailySumFertiliser() const { return _dailySumFertiliser; }
+
+		void addDailySumFertiliser(double amount)
+		{
+      _dailySumFertiliser+=amount;
+      _sumFertiliser +=amount;
+    }
+
+		double dailySumIrrigationWater() const { return _dailySumIrrigationWater; }
+
+		void addDailySumIrrigationWater(double amount)
+		{
+      _dailySumIrrigationWater += amount;
+    }
+
+		double sumFertiliser() const { return _sumFertiliser; }
+
+		void resetFertiliserCounter(){ _sumFertiliser = 0; }
+
+		void resetDailyCounter()
+		{
+      _dailySumIrrigationWater = 0.0;
+      _dailySumFertiliser = 0.0;
+    }
+
+    void applyIrrigation(double amount, double nitrateConcentration = 0,
+                         double sulfateConcentration = 0);
+
+    void applyTillage(double depth);
+
+		double get_AtmosphericCO2Concentration() const
+		{
+      return vw_AtmosphericCO2Concentration;
+    }
+
+		double get_GroundwaterDepth() const { return vs_GroundwaterDepth; }
+
+    bool writeOutputFiles() {return centralParameterProvider.writeOutputFiles; }
+
+    double avgCorg(double depth_m) const;
+    double mean90cmWaterContent() const;
+    double meanWaterContent(int layer, int number_of_layers) const;
+    double sumNmin(double depth_m) const;
+    double groundWaterRecharge() const;
+    double nLeaching() const;
+    double sumSoilTemperature(int layers);
+    double sumNO3AtDay(double depth);
+    double maxSnowDepth() const;
+    double getAccumulatedSnowDepth() const;
+    double getAccumulatedFrostDepth() const;
+    double avg30cmSoilTemperature();
+    double avgSoilMoisture(int start_layer, int end_layer);
+    double avgCapillaryRise(int start_layer, int end_layer);
+    double avgPercolationRate(int start_layer, int end_layer);
+    double sumSurfaceRunOff();
+    double surfaceRunoff();
+    double getEvapotranspiration();
+    double getTranspiration();
+    double getEvaporation();
+    double get_sum30cmSMB_CO2EvolutionRate();
+    double getNH3Volatilised();
+    double getSumNH3Volatilised();
+    double getsum30cmActDenitrificationRate();
+    double getETa();
+
+    double vw_AtmosphericCO2Concentration;
+    double vs_GroundwaterDepth;
+
+
+    /**
+     * @brief Returns soil temperature
+     * @return temperature
+     */
+		const SoilTemperature& soilTemperature() { return _soilTemperature; }
+
+    /**
+     * @brief Returns soil moisture.
+     * @return Moisture
+     */
+		const SoilMoisture& soilMoisture() { return _soilMoisture; }
+
+    /**
+     * @brief Returns soil organic mass.
+     * @return soil organic
+     */
+		const SoilOrganic& soilOrganic() { return _soilOrganic; }
+
+    /**
+     * @brief Returns soil transport
+     * @return soil transport
+     */
+		const SoilTransport& soilTransport() { return _soilTransport; }
+
+    /**
+     * @brief Returns soil column
+     * @return soil column
+     */
+		const SoilColumn& soilColumn() { return _soilColumn; }
+
+		SoilColumn& soilColumnNC() { return _soilColumn; }
+
+    /**
+     * @brief returns value for current crop.
+     * @return crop growth
+     */
+		CropGrowth* cropGrowth() { return _currentCropGrowth; }
+
+    /**
+     * @brief Returns net radiation.
+     * @param globrad
+     * @return radiation
+     */
+		double netRadiation(double globrad) { return globrad * (1 - _env.albedo); }
+
+    int daysWithCrop() {return p_daysWithCrop; }
+    double getAccumulatedNStress() { return p_accuNStress; }
+    double getAccumulatedWaterStress() { return p_accuWaterStress; }
+    double getAccumulatedHeatStress() { return p_accuHeatStress; }
+    double getAccumulatedOxygenStress() { return p_accuOxygenStress; }
+
+    double getGroundwaterInformation(Tools::Date date) {return _env.groundwaterInformation.getGroundwaterInformation(date); }
+
+  private:
+		//! environment describing the conditions under which the model runs
+    Env _env; 
+
+		//! main soil data structure
+    SoilColumn _soilColumn;
+		//! temperature code
+    SoilTemperature _soilTemperature;
+		//! moisture code
+    SoilMoisture _soilMoisture;
+		//! organic code
+    SoilOrganic _soilOrganic;
+		//! transport code
+    SoilTransport _soilTransport;
+		//! crop code for possibly planted crop
+    CropGrowth* _currentCropGrowth;
+		//! currently possibly planted crop
+    CropPtr _currentCrop;
+
+		//! store applied fertiliser during one production process
+    double _sumFertiliser;
+
+		//! stores the daily sum of applied fertiliser
+    double _dailySumFertiliser;
+
+    double _dailySumIrrigationWater;
+
+		//! climate data available to the model
+    const Climate::DataAccessor& _dataAccessor;
+
+    const CentralParameterProvider& centralParameterProvider;
+
+    int p_daysWithCrop;
+    double p_accuNStress;
+    double p_accuWaterStress;
+    double p_accuHeatStress;
+    double p_accuOxygenStress;
+  };
+
+  //----------------------------------------------------------------------------
+
+  /*!
+   * main function for running monica under a given Env(ironment)
+   * @param env the environment completely defining what the model needs and gets
+   * @return a structure with all the Monica results
+   */
+#ifndef	MONICA_GUI
+  Result runMonica(Env env);
+#else
+  Result runMonica(Env env, Configuration* cfg = NULL);
+#endif
+
+  void initializeFoutHeader(std::ofstream&);
+  void initializeGoutHeader(std::ofstream&);
+  void writeCropResults(const CropGrowth*, std::ofstream&, std::ofstream&, bool);
+	void writeGeneralResults(std::ofstream &fout, std::ofstream &gout, Env &env,
+													 MonicaModel &monica, int d);
+  void dumpMonicaParametersIntoFile(std::string, CentralParameterProvider &cpp);
+}
+
+#endif
